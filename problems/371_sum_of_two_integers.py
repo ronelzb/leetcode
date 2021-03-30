69756d36e34678d1d2fd8edfdf1b507b9a54b3ae
ronelzb <ronelzb@gmail.com>
2021-03-30 18:47:25 -0500

    solved 371 sum of two integers from problems

---
+++ problems/371_sum_of_two_integers.py
@@ -0,0 +1,25 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        def _sum(x: int, y: int):
+            while y:
+                carry = x & y
+                x ^= y
+                y = carry << 1
+
+            return x
+
+        if a * b < 0:
+            if -a == b:
+                return 0
+            if a > 0:
+                return self.getSum(b, a)
+            if -a < b:
+                return -_sum(-a, -b)
+
+        return _sum(a, b)
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    assert sol.getSum(1, 2) == 3
+    assert sol.getSum(2, 3) == 5
