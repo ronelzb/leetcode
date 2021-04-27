929ad2adc59ca107dcadea7abda208d347495e1c
ronelzb <ronelzb@gmail.com>
2021-04-26 19:13:23 -0500

    solved longest palindromic substring from blind 75 must do list

---
+++ problems/blind_75_must_do/0005_longest_palindromic_substring.py
@@ -0,0 +1,41 @@
+# https://leetcode.com/problems/longest-palindromic-substring/
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        max_length = 1
+        max_start = max_end = 0
+        n = len(s)
+
+        for i in range(1, n):
+            # check odd substring ending at i
+            start, end = i - 1, i + 1
+            while start >= 0 and end < n and s[start] == s[end]:
+                if end - start + 1 > max_length:
+                    max_start = start
+                    max_end = end
+                    max_length = end - start + 1
+
+                start -= 1
+                end += 1
+
+            # check even substring ending at i
+            start, end = i - 1, i
+            while start >= 0 and end < n and s[start] == s[end]:
+                if end - start + 1 > max_length:
+                    max_start = start
+                    max_end = end
+                    max_length = end - start + 1
+
+                start -= 1
+                end += 1
+
+        return s[max_start: max_end + 1]
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    assert sol.longestPalindrome(s="babad") == "bab" or "aba"
+    assert sol.longestPalindrome(s="cbbd") == "bb"
+    assert sol.longestPalindrome(s="a") == "a"
+    assert sol.longestPalindrome(s="ac") == "a"
+    assert sol.longestPalindrome(s="bb") == "bb"
+    assert sol.longestPalindrome(s="ccc") == "ccc"
