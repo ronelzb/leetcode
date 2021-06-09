72a9da3f0f05fe7e488dbdd37a3d6b3805adbb7d
ronelzb <ronelzb@gmail.com>
2021-06-08 22:35:03 -0500

    solved generate parentheses from 60 questions to solve

---
+++ problems/60_questions_to_solve/0022_generate_parentheses.py
@@ -0,0 +1,37 @@
+# https://leetcode.com/problems/generate-parentheses/
+# Idea: https://leetcode.com/problems/generate-parentheses/discuss/10100/Easy-to-understand-Java-backtracking-solution
+from typing import List
+
+
+class Solution:
+
+    # The goal is to print a string of “(“ ,”)” in certain order. The length of string is 2n.
+    # The constraints are that “(“s need to match “)”s.
+    # Without constraints, we just simply print out “(“ or “)” until length hits n.
+    # So the base case will be length ==2 n, recursive case is print out “(“ and “)”
+    # We need to interpret the meanings of constraints.
+    # First, the first character should be “(“.
+    # Second, at each step, you can either print “(“ or “)”, but print “)” only when there are more “(“s than “)”s.
+    # Stop printing out “(“ when the number of “(“ s hit n
+
+    def generateParenthesis(self, n: int) -> List[str]:
+        parentheses = []
+
+        def backtracking(weave, start, end) -> None:
+            if len(weave) == 2 * n:
+                parentheses.append(weave)
+                return
+
+            if start < n:
+                backtracking(weave + "(", start + 1, end)
+            if end < start:
+                backtracking(weave + ")", start, end + 1)
+
+        backtracking("", 0, 0)
+        return parentheses
+
+
+if __name__ == "__main__":
+    sol = Solution()
+
+    assert sol.generateParenthesis(n=3) == ["((()))", "(()())", "(())()", "()(())", "()()()"]
