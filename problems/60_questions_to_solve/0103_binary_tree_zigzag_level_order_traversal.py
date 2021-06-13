e9cf51d44184c7176bc4360a9e07968593b09259
ronelzb <ronelzb@gmail.com>
2021-06-13 15:57:37 -0500

    solved binary tree zigzag level order traversal from 60 questions to solve

---
+++ problems/60_questions_to_solve/0103_binary_tree_zigzag_level_order_traversal.py
@@ -0,0 +1,51 @@
+# https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
+# Definition for a binary tree node.
+from collections import deque
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def zigzagLevelOrder(self, root: TreeNode) -> List[List[int]]:
+        if root is None:
+            return []
+
+        tree_list = []
+        tree_list_len = 0
+        queue = deque([(1, root)])
+
+        while queue:
+            level, node = queue.popleft()
+
+            if level > tree_list_len:
+                tree_list.append([])
+                tree_list_len += 1
+            if level % 2 != 0:
+                tree_list[level - 1].append(node.val)
+            else:
+                tree_list[level - 1].insert(0, node.val)
+
+            if node.left:
+                queue.append((level + 1, node.left))
+            if node.right:
+                queue.append((level + 1, node.right))
+
+        return tree_list
+
+
+if __name__ == "__main__":
+    sol = Solution()
+
+    t = TreeNode(3)
+    t.left = TreeNode(9)
+    t.right = TreeNode(20)
+    t.right.left = TreeNode(15)
+    t.right.right = TreeNode(7)
+
+    print(sol.zigzagLevelOrder(t))  # [[3],[20,9],[15,7]]
