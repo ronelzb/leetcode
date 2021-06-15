174b2adb9807426c7fe3aec3b955053c15d7b685
ronelzb <ronelzb@gmail.com>
2021-06-14 20:10:27 -0500

    solved pow(x, n) from 60 questions to solve

---
+++ problems/60_questions_to_solve/0050_pow_x_n.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/powx-n/
+class Solution:
+    # Using an optimized divide-and-conquer method the function can be optimized
+    # to O(log(n)) by calculating power(x, y/2) only once and storing it.
+    def myPow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1.0
+
+        res = self.myPow(x, int(n / 2))
+        if n % 2 == 0:
+            return res * res
+        else:
+            if n > 0:
+                return x * res * res
+            else:
+                return (res * res) / x
+
+
+if __name__ == "__main__":
+    sol = Solution()
+
+    print(sol.myPow(x=2.00000, n=10))  # 1024.0
+    print(sol.myPow(x=2.10000, n=3))  # 9.261
+    print(sol.myPow(x=2.00000, n=-2))  # 0.25
