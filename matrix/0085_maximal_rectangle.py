732092e6bba33cbd9ea809a200b7321f0c7aef70
ronelzb <ronelzb@gmail.com>
2021-08-17 21:35:26 -0500

    removed prints

--- matrix/0085_maximal_rectangle.py
+++ matrix/0085_maximal_rectangle.py
@@ -29,7 +29,6 @@ class Solution:
         for row in matrix:
             for i in range(n):
                 height[i] = height[i] + 1 if row[i] == '1' else 0
-            print(height)
             stack = deque([-1])
 
             for i in range(n + 1):
@@ -38,7 +37,6 @@ class Solution:
                     w = i - 1 - stack[-1]
                     ans = max(ans, h * w)
                 stack.append(i)
-                print(stack)
 
         return ans
 
