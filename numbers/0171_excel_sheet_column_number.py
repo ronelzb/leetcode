67dea5a41218a58c94d4005fc7456ceda39e039a
ronelzb <ronelzb@gmail.com>
2021-08-30 20:20:42 -0500

    solved Excel sheet column to number

---
+++ numbers/0171_excel_sheet_column_number.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/excel-sheet-column-number/
+# tags: #numbers
+#
+# We iterate through the chars in the input and we want to take whatever is stored in res already,
+# multiply it by 26.
+#
+# Time complexity: O(n), Space complexity: O(n)
+from functools import reduce
+
+
+class Solution:
+    def titleToNumber(self, columnTitle: str) -> int:
+        return reduce(lambda a, b: a * 26 + b, [ord(c) - 64 for c in columnTitle])
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.titleToNumber(columnTitle="A"))  # 1
+    print(sol.titleToNumber(columnTitle="AB"))  # 28
+    print(sol.titleToNumber(columnTitle="ZY"))  # 701
+    print(sol.titleToNumber(columnTitle="FXSHRXW"))  # 2147483647
