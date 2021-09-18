073068b912f2c6244b23d488a0eded7101af0a00
ronelzb <ronelzb@gmail.com>
2021-09-17 22:56:27 -0500

    solved median of two sorted arrays

---
+++ array/0004_median_of_two_sorted_arrays.py
@@ -0,0 +1,41 @@
+# https://leetcode.com/problems/median-of-two-sorted-arrays/
+# tags: #tag1
+#
+# Optimal solution (if you dare):
+# https://leetcode.com/problems/median-of-two-sorted-arrays/discuss/2481/Share-my-O(log(min(mn)))-solution-with-explanation
+#
+# Solution: Find the median by traversing
+# Time Complexity: O(m+n), Space complexity: O(1)
+from typing import List
+
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        n, m = len(nums1), len(nums2)
+        ia, ma, ib, mb = 0, 0, 0, 0
+
+        for i in range((n + m) // 2 + 1):
+            ma = mb
+
+            if ia == n:
+                mb = nums2[ib]
+                ib += 1
+            elif ib == m:
+                mb = nums1[ia]
+                ia += 1
+            elif nums1[ia] < nums2[ib]:
+                mb = nums1[ia]
+                ia += 1
+            else:  # nums1[ia] >= nums2[ib]
+                mb = nums2[ib]
+                ib += 1
+
+        if (n + m) % 2 == 0:
+            return (ma + mb) / 2
+
+        return mb
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.findMedianSortedArrays(nums1=[1, 2], nums2=[3, 4]))  # 2.5
