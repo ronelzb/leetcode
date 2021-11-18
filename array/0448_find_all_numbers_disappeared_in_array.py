85a4022be38741980b23b731ff0fcb1df3bf67df
ronelzb <ronelzb@gmail.com>
2021-11-17 19:53:09 -0600

    improved and standardized documentation

--- array/0448_find_all_numbers_disappeared_in_array.py
+++ array/0448_find_all_numbers_disappeared_in_array.py
@@ -1,9 +1,14 @@
-from typing import List
-
-
+# https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+# tags: #array, #hash_table
+#
+# Solution: Two pass
 # Completeness: change nums[n-1] to -1 for all n in nums, meaning that we have seen n in nums.
 # Then we traverse nums once more, and record all indices idx such that nums[idx] != -1.
 # Each idx+1 will be a number that disappears in nums
+# Time complexity: O(n), Space complexity: O(1)
+from typing import List
+
+
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
         for n in nums:
