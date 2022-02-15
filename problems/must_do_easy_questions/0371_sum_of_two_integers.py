4c1860b7d89a248ef248be8d1e54af967c35f456
ronelzb <ronelzb@gmail.com>
2022-02-14 18:32:42 -0600

    improved and standardized documentation

--- problems/must_do_easy_questions/0371_sum_of_two_integers.py
+++ problems/must_do_easy_questions/0371_sum_of_two_integers.py
@@ -1,3 +1,9 @@
+# https://leetcode.com/problems/sum-of-two-integers/submissions/
+# tags: #math, #bit_manipulation
+#
+# Solution: Bit manipulation
+# Time complexity: O(1) It will loop at most 32 times, Space complexity: O(1)
+
 class Solution:
     def getSum(self, a: int, b: int) -> int:
         def _sum(x: int, y: int):
@@ -23,3 +29,4 @@ if __name__ == "__main__":
     sol = Solution()
     assert sol.getSum(1, 2) == 3
     assert sol.getSum(2, 3) == 5
+    assert sol.getSum(-14, 16) == 2
