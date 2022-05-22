529e1731e106f8aa0b9c9a4ac0dd1ef6e8da3401
ronelzb <ronelzb@gmail.com>
2022-05-22 14:52:55 -0500

    added tags/comments, improved optimal solution

--- string/0005_longest_palindromic_substring.py
+++ string/0005_longest_palindromic_substring.py
@@ -1,10 +1,14 @@
 # https://leetcode.com/problems/longest-palindromic-substring/
-# tags: #string
+# tags: #dp, #string
 #
-# Solution: odd + even substrings
+# Solution 1: odd + even substrings
 # For each mid point i, use two points (left, right) to check the values on i's left and right sides respectively
-#
 # Time complexity: O(n^2), Space complexity: O(1)
+#
+# Solution 2: DP
+# Bottom-up apprach: Reuse a previously computed palindrome to compute a larger palindrome
+# reverse range for i: this is bc dp[i][j] depends on dp[i+1][j-1], we need to check larger i first
+# Time complexity: O(n^2), Space complexity: O(n^2)
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         max_length = 1
@@ -36,10 +40,30 @@ class Solution:
 
         return s[max_start: max_end + 1]
 
+    def longestPalindrome_dp(self, s: str) -> str:
+        n = len(s)
+        dp = [[False] * n for _ in range(n)]
+        res = s[0]
+        max_length = 1
+
+        for i in range(n):
+            dp[i][i] = True
+
+        for start in range(n - 1, -1, -1):
+            for end in range(start + 1, n):
+                if s[start] == s[end]:
+                    if end - start == 1 or dp[start + 1][end - 1]:
+                        dp[start][end] = True
+                        if max_length < end - start + 1:
+                            max_length = end - start + 1
+                            res = s[start: end + 1]
+
+        return res
+
 
 if __name__ == "__main__":
     sol = Solution()
-    assert sol.longestPalindrome(s="babad") == "bab" or "aba"
+    assert sol.longestPalindrome_dp(s="babad") == "bab" or "aba"
     assert sol.longestPalindrome(s="cbbd") == "bb"
     assert sol.longestPalindrome(s="a") == "a"
     assert sol.longestPalindrome(s="ac") == "a"
--- string/0647_palindromic_substrings.py
+++ string/0647_palindromic_substrings.py
@@ -1,4 +1,14 @@
 # https://leetcode.com/problems/palindromic-substrings/
+# tags: #blind_75_must_do, #dp, #string
+#
+# Solution 1: odd + even substrings
+# For each mid point i, use two points (left, right) to check the values on i's left and right sides respectively
+# Time complexity: O(n^2), Space complexity: O(1)
+#
+# Solution 2: DP based on longest palindromic substring
+# Reuse a previously computed palindrome to compute a larger palindrome
+# reverse range for i: this is bc dp[i][j] depends on dp[i+1][j-1], we need to check larger i first
+# Time complexity: O(n^2), Space complexity: O(n^2)
 class Solution:
     def countSubstrings(self, s: str) -> int:
         n = len(s)
@@ -21,8 +31,6 @@ class Solution:
 
         return count_palindromes
 
-    # Hint: Reuse a previously computed palindrome to compute a larger palindrome
-    # reverse range for i: this is bc dp[i][j] depends on dp[i+1][j-1], we need to check larger i first
     def countSubstrings_dp(self, s: str) -> int:
         n = len(s)
         dp = [[False] * n for _ in range(n)]
@@ -30,13 +38,8 @@ class Solution:
 
         for i in range(n - 1, -1, -1):
             for j in range(i, n):
-                if s[i] == s[j] and (j - i < 2 or dp[i + 1][j - 1]):
-                    dp[i][j] = True
-                else:
-                    dp[i][j] = False
-
-                if dp[i][j]:
-                    count_palindromes += 1
+                dp[i][j] = s[i] == s[j] and (j - i < 2 or dp[i + 1][j - 1])
+                if dp[i][j]: count_palindromes += 1
 
         return count_palindromes
 
