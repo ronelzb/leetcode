6da495704ff9d31d6762df123b292ba866fafdd1
ronelzb <ronelzb@gmail.com>
2022-06-08 18:43:17 -0500

    solved

---
+++ string/1332_remove_palindromic_subsequences.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/break-a-palindrome/
+# tags: #two_pointers, #string
+#
+# Solution: Two pointers
+# Max 2 operations solution:
+# * if it's empty return 0
+# * if the whole word is palindrome return 1
+# * Otherwise, we need to make 2 operations
+# Time Complexity: O(n), Space complexity: O(1)
+class Solution:
+    def removePalindromeSub(self, s: str) -> int:
+        return 0 if not s else 1 if s == s[::-1] else 2
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    print(sol.removePalindromeSub("ababa"))  # 1
+    print(sol.removePalindromeSub("abb"))  # 2
+    print(sol.removePalindromeSub("baabb"))  # 2
