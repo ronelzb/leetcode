02a1f3009e9b6408ebde05afa844d45eb1ee9814
ronelzb <ronelzb@gmail.com>
2022-07-05 17:38:38 -0500

    added bit manipulation solution

--- array/0169_majority_element.py
+++ array/0169_majority_element.py
@@ -1,9 +1,13 @@
 # https://leetcode.com/problems/majority-element/
 # tags: #array, #couting, #divide_and_conquer, #hash_table, #must_do_easy_questions, #sorting
 #
-# Solution: Moore voting
+# Solution 1: Moore voting
 # Boyer–Moore majority vote algorithm (https://en.wikipedia.org/wiki/Boyer%E2%80%93Moore_majority_vote_algorithm)
 # Time complexity: O(n), Space complexity: O(1)
+#
+# Solution 2: Bit manipulation
+# We will count the majority of ones or zeroes for each bit representation number starting from LSB
+# Time complexity: O(log(w)*n) w=size of each num in nums, Space complexity: O(1)
 from typing import List
 
 
@@ -22,8 +26,23 @@ class Solution:
 
         return current
 
+    def majorityElement_bit(self, nums: List[int]) -> int:
+        n = len(nums)
+        res = 0
+
+        for i in range(32):
+            ones = 0
+            for num in nums:
+                if (1 << i) & num:
+                    ones += 1
+
+            if ones > n // 2:
+                res |= (1 << i)
+
+        return res
+
 
 if __name__ == "__main__":
     sol = Solution()
     nums = [2, 2, 1, 1, 1, 2, 2]
-    print(sol.majorityElement(nums))
+    print(sol.majorityElement_bit(nums))  # 2
