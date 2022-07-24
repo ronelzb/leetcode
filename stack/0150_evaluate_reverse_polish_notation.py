6fc365985f7403f60d036c9827b2298008375208
ronelzb <ronelzb@gmail.com>
2022-07-24 11:20:56 -0500

    Add solution headline

--- stack/0150_evaluate_reverse_polish_notation.py
+++ stack/0150_evaluate_reverse_polish_notation.py
@@ -1,12 +1,12 @@
 # https://leetcode.com/problems/evaluate-reverse-polish-notation/
-# tags: #array, #math, #stack
+# tags: #array, #google, #math, #stack
 #
+# Solution: Stack
 # Classical stack problem: Traverse the list storing each number found in the stack, when an operator is found
 # pop the latest 2 values stored and make the proper operator upon them being careful of the division operation
 # if the result number is less than zero we need to store the ceiling. Eventually store the result back into the
 # stack.
-# for the case 1/-22, python returns -1, so its recommended to use itruediv to get a float then apply int() to get 0
-#
+# For the case 1/-22, python returns -1, so its recommended to use itruediv to get a float then apply int() to get 0
 # Time Complexity: O(n), Space complexity: O(n)
 import operator
 from collections import deque
