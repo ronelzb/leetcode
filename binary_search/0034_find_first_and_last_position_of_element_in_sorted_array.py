f67185995ff80ec2ca3107d0eebe57f8ff2a6633
ronelzb <ronelzb@gmail.com>
2022-07-25 17:03:07 -0500

    add solution headline, made search method private

--- binary_search/0034_find_first_and_last_position_of_element_in_sorted_array.py
+++ binary_search/0034_find_first_and_last_position_of_element_in_sorted_array.py
@@ -1,11 +1,11 @@
 # https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
 # tags: #array, #binary_search
 #
+# Solution: Binary Search
 # Time complexity goal suggests to implement binary search.
-# However because we want the range, we need to use modified binary search, we can do the follow:
+# However, because we want the range, we need to use modified binary search, we can do the follow:
 # 1. When the nums[mid] is too small, the range starts on the right of mid, so lo = mid + 1.
-# 2. For the right boundary, we can merge the the other two conditions into one: right = middle
-#
+# 2. For the right boundary, we can merge the other two conditions into one: right = middle
 # Eventually, we can optimize our code when finding the result's right boundary (range),
 # instead of searching target, we can search target + 1.
 # Then deduct 1 from the result to get the correct position of right boundary.
@@ -16,29 +16,28 @@ from typing import List
 
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
+        def search(start: int, end: int, t: int) -> int:
+            while start < end:
+                middle = (start + end) // 2
+                if nums[middle] < t:
+                    start = middle + 1
+                else:
+                    end = middle
+            return start
+
         result = [-1, -1]
         n = len(nums)
         if n == 0:
             return result
 
-        left = self.search(nums, 0, n, target)
+        left = search(0, n, target)
         if left >= n or nums[left] != target:
             return result
 
         result[0] = left
-        result[1] = self.search(nums, left, n, target + 1) - 1
+        result[1] = search(left, n, target + 1) - 1
 
         return result
-
-    def search(self, nums: List[int], left: int, right: int, target: int) -> int:
-        while left < right:
-            middle = (left + right) // 2
-            if nums[middle] < target:
-                left = middle + 1
-            else:
-                right = middle
-
-        return left
 
 
 if __name__ == "__main__":
