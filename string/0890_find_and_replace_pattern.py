cab771aaf143fec50846cd60ebe6d2767990e641
ronelzb <ronelzb@gmail.com>
2022-07-30 12:41:03 -0500

    solved

---
+++ string/0890_find_and_replace_pattern.py
@@ -0,0 +1,38 @@
+# https://leetcode.com/problems/find-and-replace-pattern/
+# tags: #array, #hash_map, #string
+#
+# Solution: Normalise and compare
+# Based on @lee215 post:
+# https://leetcode.com/problems/find-and-replace-pattern/discuss/161288/C%2B%2BJavaPython-Normalise-Word
+# Convert each string to a base pattern and then compare them.
+# Time complexity: O(n*c) n=len(words) c=len(word), Space complexity: O(n*c)
+from collections import Counter, defaultdict
+from typing import List
+
+
+class Solution:
+    def findAndReplacePattern(self, words: List[str], pattern: str) -> List[str]:
+        def normalise(word: str) -> str:
+            char_idx = defaultdict(int)
+            idx = ord("a")
+
+            for c in word:
+                if c not in char_idx:
+                    char_idx[c] = idx
+                    idx += 1
+
+            return "".join([chr(char_idx[c]) for c in word])
+
+        pattern_matches = []
+        pattern_normalised = normalise(pattern)
+
+        for word in words:
+            if normalise(word) == pattern_normalised:
+                pattern_matches.append(word)
+
+        return pattern_matches
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    print(sol.findAndReplacePattern(words=["abc", "deq", "mee", "aqq", "dkd", "ccc"], pattern="abb"))  # ["mee","aqq"]
