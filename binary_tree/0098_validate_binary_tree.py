74baa5e0ac2c3dec1a48e3e4c4193a7967496d0e
ronelzb <ronelzb@gmail.com>
2022-08-11 19:07:13 -0500

    made code more readable

--- binary_tree/0098_validate_binary_tree.py
+++ binary_tree/0098_validate_binary_tree.py
@@ -4,34 +4,38 @@
 # Solution: DFS
 # Recursively iterating over the tree while defining interval <min_val, max_val> for each node.
 # Time complexity: O(n), Space complexity: O(n)
-class TreeNode:
-    def __init__(self, val=0):
-        self.val = val
-        self.left = None
-        self.right = None
+import sys
+from typing import Optional
+
+from utils.tree_traversal_helper import TreeNode
 
 
 class Solution:
-    def isValidBST(self, root: TreeNode, min_val: int = None, max_val: int = None) -> bool:
-        if root is None:
+    def isValidBST(
+            self,
+            root: Optional[TreeNode],
+            min_val: int = -(sys.maxsize - 1),
+            max_val: int = sys.maxsize
+    ) -> bool:
+        if not root:
             return True
 
-        if (min_val is not None and root.val <= min_val) or (max_val is not None and root.val >= max_val):
+        if root.val <= min_val or root.val >= max_val:
             return False
 
-        return self.isValidBST(root.left, min_val, root.val) and self.isValidBST(root.right, root.val, max_val)
+        return (self.isValidBST(root.left, min_val, root.val)
+                and self.isValidBST(root.right, root.val, max_val))
 
 
 if __name__ == "__main__":
     sol = Solution()
 
-    # tree_root = TreeNode(2)
-    # tree_root.left = TreeNode(1)
-    # tree_root.right = TreeNode(3)
-    #
-    # assert sol.isValidBST(tree_root)
+    t = TreeNode(2,
+                 TreeNode(1),
+                 TreeNode(3))
+    print(sol.isValidBST(t))  # True
 
-    tree_root = TreeNode(0)
-    tree_root.right = TreeNode(-1)
-
-    assert not sol.isValidBST(tree_root)
+    t = TreeNode(0,
+                 None,
+                 TreeNode(-1))
+    print(sol.isValidBST(t))  # False
